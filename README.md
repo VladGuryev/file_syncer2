d2df0924a7c69cb0d5a7fb459f50cce1c0fa42b7
Гурьев Влад <v.gurev@2Gis.ru>
2021-08-29 00:03:18 +0300

    Added support for multiple file syncs

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 (dont ask me why I need that, anyway..)
 In future I'm planning to support as many files to backup as listed in config.json
 but now the hash_db format does not allow to do it. Little fix is needed
+
+### Added multiple file sycn support (it was easy, so why not?)
+Not thoroughly tested
+
+### Confines
+Ones added to hash_bd the records with certain file_labels from config.json will be the markers for
+search of hashes of files. If they are changed after some synces happened the program
+will be executing the sync even there is no real need for it.
--- file_syncer.py
+++ file_syncer.py
@@ -19,30 +19,32 @@ def is_file_exist(filename: str) -> bool:
 
 class HashDbFile:
     isExist = None
+    db_hash_list = {}  # file_label : {file_hash_list}
 
     def __init__(self, db_file_name: str):
         self.db_file_name = db_file_name
-
-    def get_prev_hash(self, file_name: str):
-        last_hash = str()
         if is_file_exist(self.db_file_name) is False:
             self.isExist = False
             print(f"There is no {self.db_file_name} file! The clear {self.db_file_name} file will be created")
-            return last_hash
+        else:
+            with open(self.db_file_name, encoding='utf8') as f:
+                for line in f:
+                    file_label, timestamp, hash_str = line.strip().split(':', 3)
+                    if file_label in self.db_hash_list:
+                        self.db_hash_list[file_label].append(hash_str)
+                    else:
+                        self.db_hash_list[file_label] = [hash_str]
 
-        db_hash_list = []
-        with open(self.db_file_name, encoding='utf8') as f:
-            for line in f:
-                db_hash_list.append(line.strip())
-
-        self.isExist = True
-        if db_hash_list:
-            last_hash = db_hash_list[-1]
+    def get_prev_hash(self, file_label: str):
+        last_hash = str()
+        if file_label in self.db_hash_list and len(self.db_hash_list[file_label]) > 0:
+            last_hash = self.db_hash_list[file_label][-1]
         return last_hash
 
-    def add_hash(self, current_hash):
+    def add_hash(self, file_label, current_hash):
         file_handler = open(self.db_file_name, "a", encoding='utf-8')
-        file_handler.write(current_hash + "\n")
+        timestamp = datetime.now().timestamp()
+        file_handler.write(f"{file_label}:{timestamp}:{current_hash}""\n")
         return
 
 
@@ -66,12 +68,13 @@ def main():
     files_to_backup = config["backup_files"]
 
     for key in files_to_backup:
-        file_data = files_to_backup[key]
+        file_label = key
+        file_data = files_to_backup[file_label]
         source_path = str(file_data["source"])
         dest_path = str(file_data["dest"])
 
         if is_file_exist(source_path) is False:
-            print(f"source file with label: {key} doesn't exist")
+            print(f"source file with label: {file_label} doesn't exist")
             return
 
         current_hash = calculate_hash(source_path)
@@ -81,14 +84,14 @@ def main():
         source_file_name = path_leaf(file_name_without_ext)
         dest_path = str(f"{dest_path}{source_file_name}_{writing_time}{source_file_extension}")
 
-        prev_hash = hash_db.get_prev_hash(source_file_name)
+        prev_hash = hash_db.get_prev_hash(file_label)
 
         if current_hash != prev_hash:
             copyfile(source_path, dest_path)
-            hash_db.add_hash(current_hash)
-            print(f"file with label \"{key}\" synced")
+            hash_db.add_hash(file_label, current_hash)
+            print(f"file with label \"{file_label}\" synced")
         else:
-            print(f"file with label \"{key}\" has already been synced")
+            print(f"file with label \"{file_label}\" has already been synced")
 
     print("end file backup")
     return 0
